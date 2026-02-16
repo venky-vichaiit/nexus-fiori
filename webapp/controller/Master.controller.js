80b6f6b98e35b6687ade0c0b46536433dddb2a5f
Venkata Inti <venkat.inti@piloggroup.com>
2026-02-16 15:52:29 +0000

    expand tree node changes

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -63,7 +63,7 @@ sap.ui.define([
                         aRows = aRows.map(function (oRow) {
                             var sAssetName = oRow.Name || oRow.Full_location || oRow.Full_Location || oRow.full_location || oRow.FullLocation || "";
                             var bHasChild = oRow.Has_Children === true;
-                            var aChildRows = bHasChild ? [{ rows: [{}] }] : [{}];
+                            var aChildRows = bHasChild ? [{ rows: [] }] : [];
                             return Object.assign({}, oRow, {
                                 Name: sAssetName,
                                 Has_Children: bHasChild,
@@ -78,6 +78,58 @@ sap.ui.define([
                         this.setBusyOff();
                     }.bind(this)
                 });
+        },
+
+        onToggleOpenState: function (oEvent) {
+            var iRowIndex = oEvent.getParameter("rowIndex");
+            var oContext = oEvent.getParameter("rowContext");
+            var bExpanded = oEvent.getParameter("expanded");
+            if (!bExpanded || !oContext) {
+                return;
+            }
+            var oSelectedRow = oContext.getProperty(oContext.getPath());
+            if (!oSelectedRow || !oSelectedRow.Has_Children) {
+                return;
+            }
+            // Skip if children are already loaded
+            if (oSelectedRow.rows && oSelectedRow.rows.length > 0 && oSelectedRow.rows[0].VN_ID) {
+                return;
+            }
+            var sCvId = oSelectedRow.CV_ID;
+            var sRootVnId = oSelectedRow.VN_ID;
+            var sPath = oContext.getPath() + "/rows";
+
+            this.setBusyOn();
+            $.ajax({
+                "url": "/bo/View_Node/",
+                "method": "GET",
+                "dataType": "json",
+                "headers": {
+                    "X-NEXUS-Filter": '{"where":[{"field":"CV_ID","method":"eq","value":"' + sCvId + '"}, {"field":"Link_ID","value":' + sRootVnId + '}]}'
+                },
+                "data": {
+                    "hash": this.hash
+                },
+                "success": function (response) {
+                    var aRows = Array.isArray(response && response.rows) ? response.rows : [];
+                    aRows = aRows.map(function (oRow) {
+                        var sAssetName = oRow.Name || oRow.Full_location || oRow.Full_Location || oRow.full_location || oRow.FullLocation || "";
+                        var bHasChild = oRow.Has_Children === true;
+                        var aChildRows = bHasChild ? [{ rows: [] }] : [];
+                        return Object.assign({}, oRow, {
+                            Name: sAssetName,
+                            Has_Children: bHasChild,
+                            rows: aChildRows
+                        });
+                    });
+                    this.getLocalDataModel().setProperty(sPath, aRows);
+                    this.setBusyOff();
+                }.bind(this),
+                "error": function (errorData) {
+                    MessageBox.error("Error while fetching child nodes");
+                    this.setBusyOff();
+                }.bind(this)
+            });
         }
     });
 });
